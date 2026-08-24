c73b444cc12955c7d95279d7a653319d4f2edc7a
Justin Brantley <fascinatingfingers@icloud.com>
2026-08-23 19:36:00 -0700

    Add developer utilities

--- R/micromegas-package.R
+++ R/micromegas-package.R
@@ -2,6 +2,26 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom covr coverage_to_list
+#' @importFrom covr file_report
+#' @importFrom covr package_coverage
+#' @importFrom devtools check
+#' @importFrom devtools document
+#' @importFrom fs dir_copy
+#' @importFrom fs dir_create
+#' @importFrom fs dir_ls
+#' @importFrom fs file_move
+#' @importFrom fs file_temp
+#' @importFrom fs path
+#' @importFrom pkgdown build_site
+#' @importFrom rstudioapi viewer
+#' @importFrom styler style_pkg
 #' @importFrom tibble tibble
+#' @importFrom usethis use_r
+#' @importFrom usethis use_test
+#' @importFrom usethis use_tidy_description
+#' @importFrom utils capture.output
+#' @importFrom utils head
+#' @importFrom yaml read_yaml
 ## usethis namespace: end
 NULL
