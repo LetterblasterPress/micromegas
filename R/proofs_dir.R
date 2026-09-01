77b7e22b5783a9412b0a2cba62e9919757e32c6b
Justin Brantley <fascinatingfingers@icloud.com>
2026-09-01 09:00:47 -0700

    Need not create dir when calling proofs_dir()

--- R/proofs_dir.R
+++ R/proofs_dir.R
@@ -1,4 +1,3 @@
 proofs_dir <- function() {
-  y <- dir_create(Sys.getenv("proofs_dir", "~/pdf_proofs/"))
-  return(y)
+  path(Sys.getenv("proofs_dir", "~/pdf_proofs/"))
 }
--- tests/testthat/test-proofs_dir.R
+++ tests/testthat/test-proofs_dir.R
@@ -1,19 +1,13 @@
 test_that("it returns path to `~/pdf_proofs` if no environment variable", {
   expect_identical(
     withr::with_envvar(c(proofs_dir = NA), proofs_dir()),
-    fs::path_expand(path("~/pdf_proofs"))
+    path("~/pdf_proofs")
   )
 })
 
 test_that("it returns custom path if environment variable set", {
-  tmp <- fs::file_temp()
-  on.exit(unlink(tmp))
-  expect_false(fs::dir_exists(tmp))
-
   expect_identical(
-    withr::with_envvar(c(proofs_dir = tmp), proofs_dir()),
-    fs::path_expand(path(tmp))
+    withr::with_envvar(c(proofs_dir = "test_proofs_dir"), proofs_dir()),
+    path("test_proofs_dir")
   )
-
-  expect_true(fs::dir_exists(tmp))
 })
